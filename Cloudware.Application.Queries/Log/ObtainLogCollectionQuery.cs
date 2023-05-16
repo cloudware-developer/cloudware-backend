9607c13fd0c0a2e279e452e099016cd97dec89e7
Rodrigo <rodrigo_arf@outlook.com>
2023-05-16 00:25:36 -0300

    .

---
+++ Cloudware.Application.Queries/Log/ObtainLogCollectionQuery.cs
@@ -0,0 +1,50 @@
+﻿using Cloudware.Core.Infra.Enums;
+using Cloudware.Core.Infra.Pagination;
+using Cludware.Repository.Entities;
+using MediatR;
+
+namespace Cloudware.Application.Queries.Log
+{
+    public class ObtainLogCollectionQuery : 
+        IRequest<List<LogAuthenticationEntity>>, 
+        ISortable, 
+        IPaginable, 
+        IQuery<List<LogAuthenticationEntity>>
+    {
+        public int LogId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string? Serialized { get; set; }
+
+        public ETypeLog TypeLog { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public Pagination Pagination { get; set; } = new Pagination();
+
+        public SortField SortField { get; set; } = new SortField();
+
+        public ObtainLogCollectionQuery() { }
+
+        public ObtainLogCollectionQuery(int logId, int userId, string? serialized, ETypeLog typeLog, DateTime? createdAt)
+        {
+            LogId = logId;
+            UserId = userId;
+            Serialized = serialized;
+            TypeLog = typeLog;
+            CreatedAt = createdAt;
+        }
+
+        public ObtainLogCollectionQuery(int logId, int userId, string? serialized, ETypeLog typeLog, DateTime? createdAt, Pagination pagination, SortField sortField)
+        {
+            LogId = logId;
+            UserId = userId;
+            Serialized = serialized;
+            TypeLog = typeLog;
+            CreatedAt = createdAt;
+            Pagination = pagination;
+            SortField = sortField;
+        }
+    }
+}
---
+++ Cloudware.Application.Queries/Log/ObtainLogQuery.cs
@@ -0,0 +1,21 @@
+﻿using Cloudware.Core.Infra.Enums;
+using Cludware.Repository.Entities;
+using MediatR;
+
+namespace Cloudware.Application.Queries.Log
+{
+    public class ObtainLogQuery : IRequest<LogAuthenticationEntity>
+    {
+        public int LogId { get; set; }
+
+        public int TypeLog { get; set; }
+
+        public ObtainLogQuery() { }
+
+        public ObtainLogQuery(int logId, int typeLog)
+        {
+            LogId = logId;
+            TypeLog = typeLog;
+        }
+    }
+}
